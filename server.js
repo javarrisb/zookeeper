d2d6a8a5d043ba268c9255d6c2ce7a49cb2e9ffc
Javarris Barnett <javarrisb@gmail.con>
2021-12-28 04:25:15 +0100

    Test the functions

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
